1ccdace8b75295cc1fabc03da58c8d533f63ea72
RANSHER RAJ <44178727+ransherraj@users.noreply.github.com>
2021-09-02 18:11:22 +0530

    Update 1. print_1-n_values_for_n.cpp

--- 1. print_1-n_values_for_n.cpp
+++ 1. print_1-n_values_for_n.cpp
@@ -1,3 +1,4 @@
+// code 1
 
 #include <iostream>
 #include <bits/stdc++.h>
@@ -7,6 +8,31 @@ using namespace std;
 int print( int n){
     if(n==0){
         return -1;
+    }
+    print(n-1);
+    cout<<n<<" ";
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    print(n);
+    return 0;
+}
+
+
+// code 2
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void print( int n){
+    if(n==1){
+        cout<<n<<" ";
+        return;
     }
     print(n-1);
     cout<<n<<" ";
