c1828d8c343a2435eea957f9306b0ddb813cf67c
RANSHER RAJ <44178727+ransherraj@users.noreply.github.com>
2021-09-02 18:01:40 +0530

    Create 1. print_1-n_values_for_n.cpp

---
+++ 1. print_1-n_values_for_n.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int print( int n){
+    if(n==0){
+        return -1;
+    }
+    print(n-1);
+    cout<<n<<" ";
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    print(n);
+    return 0;
+}
