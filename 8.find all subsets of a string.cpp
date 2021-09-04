7bb81e521f8c1510a3dc207e23bc2af94cc49d88
RANSHER RAJ <44178727+ransherraj@users.noreply.github.com>
2021-09-04 23:03:50 +0530

    Create 8.find all subsets of a string.cpp

---
+++ 8.find all subsets of a string.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(string ip, string op, int &count){
+    
+    if(ip.length() == 0){
+        cout<<op<<" ";
+        count++;
+        return;
+    }
+    
+    string op1 = op;
+    string op2 = op;
+    op2.push_back(ip[0]);
+    ip.erase(ip.begin()+0);
+    
+    solve(ip, op1, count);
+    solve(ip, op2, count);
+    return ;
+}
+
+
+int main()
+{
+    string op = "";
+    string ip;
+    cin>>ip;
+    int count = 0;
+    solve(ip, op, count);
+    cout<<endl<<"total permutations: "<<count;
+    
+}
